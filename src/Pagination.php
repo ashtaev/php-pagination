a4a74f67b1861ac2e3b888446d237bc01c8dba54
Vladislav <vl.ashtaev@gmail.com>
2020-06-16 16:56:01 +0300

    Added phpdocs

--- src/Pagination.php
+++ src/Pagination.php
@@ -4,110 +4,112 @@ namespace ashtaev;
 
 class Pagination {
 
-    public static $pages = [];
+    public $pages = Array();
 
+    /**
+     * Get an array of paginated page data.
+     *
+     * @param int $total The total number of items
+     * @param int $page The current page number
+     * @param string $path A URL for each page
+     * @param int $list The number of items per page
+     * @param int $size Range
+     * @return array|bool
+     */
+    public function get($total, $page, $path, $list = 10, $size = 6) {
 
-    private static function disable(String $i) {
-        $pp['num'] = $i;
-        $pp['url'] = false;
-        self::$pages[] = $pp;
-    }
+        $link = $total % $list == 0 ? $total/$list : ceil($total/$list);
 
-
-
-    private static function enable(String $path, $i) {
-        $pp['num'] = $i;
-        $pp['url'] = static::url($path, $i);
-        self::$pages[] = $pp;
-    }
-
-
-
-    public static function run(Int $total, Int $list, Int $page, String $path, Int $size = 6) {
-
-
-        $link = $total % $list == 0 ? $link = $total/$list : $link = ceil($total/$list);
-
-        if ($link === 1) return 0;
-
+        if ($link === 1) return false;
         $x = $size;
-
         $total_x = $x * 2 + 1;
 
         if ($link <= $total_x) {
             for ($i = 1; $i <= $link; $i++) {
                 switch ($i) {
                     case $page:
-                        self::disable($i);
+                        $this->disable($i);
                         break;
                     case 1:
-                        self::enable($path, 1);
+                        $this->enable($path, 1);
                         break;
                     default:
-                        self::enable($path, $i);
+                        $this->enable($path, $i);
                 }
             }
-
         } else {
-
             if ($x + 1 >= $page) {
-
                 for ($i = 1; $i <= $total_x - 2; $i++) {
                     switch ($i) {
                         case $page:
-                            self::disable($i);
+                            $this->disable($i);
                             break;
                         case 1:
-                            self::enable($path, 1);
+                            $this->enable($path, 1);
                             break;
                         default:
-                            self::enable($path, $i);
+                            $this->enable($path, $i);
                     }
                 }
-                self::disable("…");
-                self::enable($path, $link);
-
+                $this->disable("…");
+                $this->enable($path, $link);
             } elseif ($page < $link - $x) {
-
-                self::enable($path, 1);
-                self::disable("…");
+                $this->enable($path, 1);
+                $this->disable("…");
                 for ($i = $page - $x + 2; $i != $page; $i++) {
-                    self::enable($path, $i);
+                    $this->enable($path, $i);
                 }
-                self::disable($i);
+                $this->disable($i);
                 for ($i = $page + 1; $i <= $page + $x - 2; $i++) {
-                    self::enable($path, $i);
+                    $this->enable($path, $i);
                 }
-                self::disable("…");
-                self::enable($path, $link);
-
+                $this->disable("…");
+                $this->enable($path, $link);
             } else {
-
-                self::enable($path, 1);
-                self::disable("…");
-
+                $this->enable($path, 1);
+                $this->disable("…");
                 for ($i = $link - $total_x + 3; $i <= $link; $i++) {
                     switch ($i) {
                         case $page:
-                            self::disable($i);
+                            $this->disable($i);
                             break;
                         case 1:
-                            self::enable($path, 1);
+                            $this->enable($path, 1);
                             break;
                         default:
-                            self::enable($path, $i);
+                            $this->enable($path, $i);
                     }
                 }
             }
         }
-
-        return self::$pages;
+        return $this->pages;
     }
 
-
-
-    protected static function url($path, $i = 1) {
-
+    /**
+     * @param int $i
+     */
+    private function disable($i) {
+        $pp['num'] = $i;
+        $pp['url'] = false;
+        $this->pages[] = $pp;
     }
 
+    /**
+     * @param string $path
+     * @param int $i
+     */
+    private function enable($path, $i) {
+        $pp['num'] = $i;
+        $pp['url'] = $this->url($path, $i);
+        $this->pages[] = $pp;
+    }
+
+    /**
+     * @param string $path
+     * @param int $i
+     * @return string
+     */
+    protected function url($path, $i = 1) {
+        return str_replace("[:page]", $i, $path);
+    }
 }
